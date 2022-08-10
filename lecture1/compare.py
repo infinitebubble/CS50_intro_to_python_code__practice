67a841e34c72cb953fb3e74bae81f008fdc2cc71
Nitin Kashyap <kashyapnitin003@rediffmail.com>
2022-08-10 21:07:24 +0530

    comparison operators

---
+++ lecture1/compare.py
@@ -0,0 +1,38 @@
+x = int(input("x : "))
+y = int(input("y : "))
+
+# basic if lese conditions
+"""
+if x < y:
+    print("x i less then y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x equals y")
+"""
+
+# use elif : making utually exclusive conditions
+"""if x < y:
+    print("x is less than y")
+elif x > y:
+    print(" x is greater")
+elif x == y:
+    print(" x equals y")
+"""
+#  using else
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print(" x is greater")
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print(" x is greater")
+else:
+    print(" x equals y")
+
+
+# using not operator
+
+if x != y:
+    print("x is not quals y")
